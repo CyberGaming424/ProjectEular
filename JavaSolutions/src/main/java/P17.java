4cb1ba9ef8299cffcb56b933b1a49cb9cf4c7f38
BuildTools <jlake624@gmail.com>
2022-11-14 21:17:09 -0500

    non-working P17 solution

--- JavaSolutions/src/main/java/P17.java
+++ JavaSolutions/src/main/java/P17.java
@@ -22,16 +22,8 @@ class P17 {
       "fifteen",
       "sixteen",
       "seventeen",
-      "eightteen",
-      "nineteen",
-      "twenty",
-      "thirty",
-      "fourty",
-      "fifty",
-      "sixty",
-      "seventy",
-      "eighty",
-      "ninety"
+      "eighteen",
+      "nineteen"
   };
 
   String hAnd = "hundredand";
@@ -43,12 +35,55 @@ class P17 {
     }
 
     for (int i = 10; i <= 1000; i++) {
+      String[] num = String.valueOf(i).split("");
       if (i < 20) {
         len += dubDig[i - 10].length();
-      } else {
-        String[] num = String.valueOf(i).split("");
+      } else if(i >= 20 && i < 100){
+        len+=dubDig(num[0]);
+        if(!num[1].equalsIgnoreCase("0")){
+          len+=singDig[Integer.parseInt(num[1])].length();
+        }
+      }else {
+        if(num[1].equalsIgnoreCase("0")){
+          len+=singDig[Integer.parseInt(num[0])].length() + hAnd.length() + singDig[Integer.parseInt(num[2])].length();
+        }else{
+          len+=singDig[Integer.parseInt(num[0])].length() + hAnd.length() + dubDig(num[1]) +singDig[Integer.parseInt(num[2])].length();
+        }
       }
     }
+    System.out.println(len+"onethousand".length());
 
   }
+
+  private int dubDig(String dig){
+    int len = 0;
+    switch (dig){
+      case "2":
+        len+="twenty".length();
+        break;
+      case "3":
+        len+="thirty".length();
+        break;
+      case "4":
+        len+="forty".length();
+        break;
+      case "5":
+        len+="fifty".length();
+        break;
+      case "6":
+        len+="sixty".length();
+        break;
+      case "7":
+        len+="seventy".length();
+        break;
+      case "8":
+        len+="eighty".length();
+        break;
+      case "9":
+        len+="ninety".length();
+        break;
+    }
+    return len;
+  }
+
 }
